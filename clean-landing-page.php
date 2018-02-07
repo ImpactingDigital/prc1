aa216ccf7e0acffac5baf16833fd2213dbc265c0
David Siqueira Pereira <dvdleaf@gmail.com>
2018-02-07 11:04:14 +0000

    google tag manager

--- clean-landing-page.php
+++ clean-landing-page.php
@@ -26,45 +26,27 @@
 		<meta property="og:image" content="https://skills.primaveraacademy.com/wp-content/uploads/2017/11/primavera-academy-rocket-image.jpg">
 		<meta property="og:description" content="Dê o próximo passo sua carreira">
 
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109126622-1"></script>
-		<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'UA-109126622-1');
-		</script>
-
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,700" rel="stylesheet">
-				
 		<?php wp_head(); ?>
-
-
-		<!-- Facebook Pixel Code -->
-		<script>
-		!function(f,b,e,v,n,t,s)
-		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-		n.queue=[];t=b.createElement(e);t.async=!0;
-		t.src=v;s=b.getElementsByTagName(e)[0];
-		s.parentNode.insertBefore(t,s)}(window,document,'script',
-		'https://connect.facebook.net/en_US/fbevents.js');
-		fbq('init', '1743244072654630'); 
-		fbq('track', 'PageView');
-		</script>
-		<noscript>
-		<img height="1" width="1" src="https://www.facebook.com/tr?id=1743244072654630&ev=PageView&noscript=1"/>
-		</noscript>
-		<!-- End Facebook Pixel Code -->
+        
+        
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NH5WM66');</script>
+        <!-- End Google Tag Manager -->
 		
 
 		<script type="text/javascript" src="//app.p.smrk.io/mcf.min.js"></script>
 	</head>
 
 	<body <?php body_class(); ?>>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NH5WM66"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+        
 		<div id="page" class="landing-page">
 			<a class="skip-link screen-reader-text" href="#content">
 				<?php _e( 'Skip to content', 'twentyseventeen' ); ?>
